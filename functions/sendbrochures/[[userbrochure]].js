0c359a6be0d9e256d5601341d43bab0c75be4cc5
crescolincs <157811835+crescolincs@users.noreply.github.com>
2024-04-21 08:59:22 +0100

    add code to [[userbrochure]].js

--- functions/sendbrochures/[[userbrochure]].js
+++ functions/sendbrochures/[[userbrochure]].js
@@ -1,5 +1,8 @@
+export function onRequest(context) {
 // get the user and the brochure from the dynamic url
 
 // inform airtable the [user] has requested the [brochure]
 
 // ridirect to the brochure pdf
+    return Response.redirect('https://brochures.pages.dev/brochurespdf/'+context.params.userbrochure[1]+'-brochure.pdf');
+  }
