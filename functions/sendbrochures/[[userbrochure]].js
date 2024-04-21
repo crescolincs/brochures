ffc826531ddc2b72d5cfd4bedc21e3c3931dda23
crescolincs <157811835+crescolincs@users.noreply.github.com>
2024-04-21 11:57:11 +0100

    brochure request store and redirect

--- functions/sendbrochures/[[userbrochure]].js
+++ functions/sendbrochures/[[userbrochure]].js
@@ -1,8 +1,23 @@
-export function onRequest(context) {
-// get the user and the brochure from the dynamic url
-
-// inform airtable the [user] has requested the [brochure]
-
-// ridirect to the brochure pdf
+export async function onRequest(context) {
+  // get the user and the brochure from the dynamic url  
+     var airtablejson =`{
+       "fields": {
+        "who": ["${context.params.userbrochure[0]}"], 
+        "brochure": "${context.params.userbrochure[1]}"
+      }
+    }`
+     // write to airtable table [brochures-downloaded] the [user] has requested the [brochure]
+    const airtableresponse = await fetch(`https://api.airtable.com/v0/appTDInc67J2LFy2g/tblXWXccP64yQR9iv`, {
+    method: 'POST',
+    body: airtablejson,
+    headers: {
+       'Authorization': 'Bearer '+context.env.AT_TKN,
+       'Content-Type': 'application/json',
+       'X-Requested-With': 'XMLHttpRequest', 
+      }    
+    })
+    const airtableJSON = await airtableresponse.json()
+    //console.log("airtableJSON : "+JSON.stringify(airtableJSON))
+    // ridirect to the brochure pdf
     return Response.redirect('https://brochures.pages.dev/brochures/'+context.params.userbrochure[1]+'-brochure.pdf');
   }
